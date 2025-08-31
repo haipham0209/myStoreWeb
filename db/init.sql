6a4494998ef12fbccbab4d683f696dcf2e581aa7
NGOCHAI <phamngochai0209@gmail.com>
2025-08-31 22:09:26 +0900

    categoryid でーたかたが

--- db/init.sql
+++ db/init.sql
@@ -53,6 +53,10 @@ CREATE TABLE category (
 	    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	    FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE
 	);
+
+    ALTER TABLE category MODIFY category_id BIGINT NOT NULL AUTO_INCREMENT;
+    ALTER TABLE product MODIFY category_id BIGINT NOT NULL;
+
 	
 	-- Giảm giá
 	CREATE TABLE discounts (
